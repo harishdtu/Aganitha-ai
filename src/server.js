bf930c9209427eb4c7a73cf7fb140c3446b67974
harishdtu <satyaharish12345@gmail.com>
2026-01-31 22:21:27 +0530

    Fix Express server for Vercel serverless

--- src/server.js
+++ src/server.js
@@ -1,9 +1,4 @@
 require("dotenv").config();
-require("dns").setDefaultResultOrder("ipv4first");
 const app = require("./app");
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+module.exports = app;
